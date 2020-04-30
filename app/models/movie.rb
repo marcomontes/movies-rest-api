344ab624ccd501613d0b9694856597150c6e744c
Marco Montes <marcomontes@gmail.com>
2020-04-30 14:52:32 -0500

    delete dependent roles on person/movie destroy

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,9 @@
 class Movie < ApplicationRecord
 
   has_many :roles
-  has_many :casting,   -> { where(roles: { name: 'Actor/Actress' })}, through: :roles, source: :person
-  has_many :directors, -> { where(roles: { name: 'Director' })}, through: :roles, source: :person
-  has_many :producers, -> { where(roles: { name: 'Producer' })}, through: :roles, source: :person
+  has_many :casting,   -> { where(roles: { name: 'Actor/Actress' })}, through: :roles, source: :person, dependent: :delete_all
+  has_many :directors, -> { where(roles: { name: 'Director' })}, through: :roles, source: :person, dependent: :delete_all
+  has_many :producers, -> { where(roles: { name: 'Producer' })}, through: :roles, source: :person, dependent: :delete_all
 
   validates :title, presence: true
   validates :release_year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1900, less_than_or_equal_to: Time.now.year }, length: { is: 4 }
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,9 @@
 class Person < ApplicationRecord
 
   has_many :roles
-  has_many :movies_as_actor,    -> { where(roles: { name: 'Actor/Actress' })}, through: :roles, source: :movie
-  has_many :movies_as_director, -> { where(roles: { name: 'Director' })}, through: :roles, source: :movie
-  has_many :movies_as_producer, -> { where(roles: { name: 'Producer' })}, through: :roles, source: :movie
+  has_many :movies_as_actor,    -> { where(roles: { name: 'Actor/Actress' })}, through: :roles, source: :movie, dependent: :delete_all
+  has_many :movies_as_director, -> { where(roles: { name: 'Director' })}, through: :roles, source: :movie, dependent: :delete_all
+  has_many :movies_as_producer, -> { where(roles: { name: 'Producer' })}, through: :roles, source: :movie, dependent: :delete_all
 
   validates :first_name, :last_name, presence: true
 
